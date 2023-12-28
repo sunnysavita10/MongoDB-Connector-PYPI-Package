25d79ac8f10cb47838637ac579c27b7bd3e3cf5d
sunnysavita10 <engsunny3365@gmail.com>
2023-12-28 16:08:33 +0530

    code updated

---
+++ README.md
@@ -0,0 +1,15 @@
+# neurolab-mongo-python
+
+![image](https://user-images.githubusercontent.com/57321948/196933065-4b16c235-f3b9-4391-9cfe-4affcec87c35.png)
+
+### Step 1 - Install the requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+### Step 2 - Run main.py file
+
+```bash
+python main.py
+```
---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+package_name = "mongodb_connect"
+
+list_of_files = [
+   ".github/workflows/ci.yaml",
+   "src/__init__.py",
+   f"src/{package_name}/__init__.py", 
+   f"src/{package_name}/mongo_crud.py", 
+   "tests/__init__.py",
+   "tests/unit/__init__.py",
+   "tests/unit/unit.py",
+   "tests/integration/__init__.py",
+   "tests/integration/int.py",
+   "init_setup.sh",
+   "requirements.txt", 
+   "requirements_dev.txt",
+   "setup.py",
+   "setup.cfg",
+   "pyproject.toml",
+   "tox.ini",
+   "experiments/experiments.ipynb", 
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # create an empty file
