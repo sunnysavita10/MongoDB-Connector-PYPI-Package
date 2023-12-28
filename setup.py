ee483449fc8dfba65c2152dd7157bd17793de41e
SUNNY SAVITA <engsunny3365@gmail.com>
2023-12-28 23:46:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,5 +38,5 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requiremet("./requirements_dev.txt"),
+    install_requires=get_requiremet("./requirements.txt"),
 )
