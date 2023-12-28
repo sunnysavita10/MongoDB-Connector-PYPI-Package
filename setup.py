a695a4cc1714d57525514b5fcfb06548b07566b0
SUNNY SAVITA <engsunny3365@gmail.com>
2023-12-29 00:29:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
 from setuptools import setup, find_packages
 from typing import List
-
-HYPEN_E_DOT='-e .'
-
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
-        
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
-
    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
@@ -37,6 +24,5 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
-    install_requires=get_requiremet("requirements.txt"),
+    packages=find_packages(where="src")
 )
