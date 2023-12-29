24070b0da18ddca0c8c09ac1b27054e8a59a8604
sunnysavita10 <engsunny3365@gmail.com>
2023-12-29 16:38:14 +0530

    code and version updated

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "mongoDBconnect"
+PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "sunnysavita10"
 AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
 
